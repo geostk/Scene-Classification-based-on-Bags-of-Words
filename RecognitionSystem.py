0353255af5f2a39d76e54ad47abf492a04e3974c
skfory <skfory@163.com>
2017-01-14 22:30:12 +0800

    fixed a bug in getImageFeaturesSPM function

--- RecognitionSystem.py
+++ RecognitionSystem.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pdb
 import cPickle
-import dictionary 
-import filterbank
 from PIL import Image
 import matplotlib.pyplot as plt
 import scipy.io as sio
@@ -11,9 +9,11 @@ import os
 from multiprocessing import Pool
 from scipy import stats
 from config import cfg
+
+
 def getImageFeatures(WordMap,DictionarySize):
     """
-    Descriptions:
+    Description:
     extract the histogram of visual words within the given image
     input:
         WordMap: A H x W image containing the IDs of visual words within the 
@@ -27,11 +27,12 @@ def getImageFeatures(WordMap,DictionarySize):
     WordMap=np.reshape(WordMap,(numOfPixel,))
     for i in xrange(numOfPixel):
         hist[WordMap[i]]=hist[WordMap[i]]+1
-    return hist/numOfPixel
+    hist = hist/numOfPixel
+    return hist
 
 def getImageFeaturesSPM(LayerNum, WordMap, DictionarySize):
     """
-    Descriptions:
+    Description:
     Form a multi-resolution representation of a given wordMap based on the 
     Spatial Pyramid Matching
     input:
@@ -46,22 +47,26 @@ def getImageFeaturesSPM(LayerNum, WordMap, DictionarySize):
 
     """
     L=LayerNum-1
+
     (H,W) = WordMap.shape
     CellHeight=H/(2**L)
     CellWidth=W/(2**L)
     LayerHist=np.zeros(((4**L)*DictionarySize,))
     SavedHist=np.zeros((4**L,DictionarySize)) # Save finer layer histograms for computing the next layer
     NewHist=np.zeros((4**L,DictionarySize)) # Save layer histograms for updating saved_hist
-    for i in xrange(2**L):   #1:2^L
-        for j in xrange(2**L):#j=1:2^L
+    for i in xrange(2**L):   
+        for j in xrange(2**L):
             StartColum=i*CellWidth
             StartRow=j*CellHeight
             CurrentHist = getImageFeatures(WordMap[StartRow:StartRow+CellHeight,StartColum:StartColum+CellWidth],DictionarySize)
-            LayerHist[i*(2**L)+j*DictionarySize:i*(2**L)+j*DictionarySize+DictionarySize]=CurrentHist
+            LayerHist[i*(2**L)*DictionarySize+j*DictionarySize:i*(2**L)*DictionarySize+j*DictionarySize+DictionarySize]=CurrentHist
+            #print 'sum(CurrentHist)=',np.sum(CurrentHist)
+            #print 'sum(LayerHist)=',np.sum(LayerHist)
             SavedHist[i*(2**L)+j,:]=CurrentHist
             
 # normalize
     LayerHist = LayerHist / (4**L)
+    #print (4**L),'sum LayerHist = LayerHist / (4**L)= ', np.sum(LayerHist)
     if(L>1):
         weight=2**(-1)
     else:
@@ -72,19 +77,19 @@ def getImageFeaturesSPM(LayerNum, WordMap, DictionarySize):
 
 
     for l in reversed(range(L)):# L-1:-1:0
-       
-        LayerHist=np.zeros(((4**L)*DictionarySize))
         
-        for i in xrange(1,2**l+1):#1:2^l
-            for j in xrange(1,2**l+1):#j=1:2^l
-                NewHist[(i-1)*(2**l)+j-1,:] = \
-                    SavedHist[0+(i-1)*(2**(l+2))+(j-1)*2,:]+ \
-                    SavedHist[1+(i-1)*(2**(l+2))+(j-1)*2,:]+ \
-                    SavedHist[0+(i-1)*(2**(l+2))+(j-1)*2+2**(l+1),:]+ \
-                    SavedHist[1+(i-1)*(2**(l+2))+(j-1)*2+2**(l+1),:]
+        LayerHist=np.zeros(((4**l)*DictionarySize))
+        
+        for i in xrange(0,2**l):#1:2^l
+            for j in xrange(0,2**l):#j=1:2^l
+                NewHist[i*(2**l)+j,:] = \
+                    SavedHist[0+(i)*(2**(l+2))+(j)*2,:]+ \
+                    SavedHist[1+(i)*(2**(l+2))+(j)*2,:]+ \
+                    SavedHist[0+(i)*(2**(l+2))+(j)*2+2**(l+1),:]+ \
+                    SavedHist[1+(i)*(2**(l+2))+(j)*2+2**(l+1),:]
                 
-                LayerHist[i*(2**L)+j*DictionarySize:i*(2**L)+\
-                j*DictionarySize+DictionarySize]= NewHist[(i-1)*(2**l)+j-1,:]/4
+                LayerHist[i*(2**l)*DictionarySize+j*DictionarySize:i*(2**l)*DictionarySize+\
+                j*DictionarySize+DictionarySize]= NewHist[(i)*(2**l)+j,:]/4
             
        
         SavedHist=NewHist/4;
@@ -95,13 +100,14 @@ def getImageFeaturesSPM(LayerNum, WordMap, DictionarySize):
         
         #normalize
         LayerHist =LayerHist / (4**l);
+        #print 'this sum =',np.sum(LayerHist)
         Hist=np.hstack((LayerHist*weight,Hist));
-    #pdb.set_trace()    
+    #pdb.set_trace()  
     return Hist
 
 def distanceToSet(WordHist, Histograms):
     """
-    Descriptions:
+    Description:
         returns the histogram intersection similarity between WordHist and each 
         training sample as a 1XT vector(T is the number of training samples)
         intput 
@@ -131,8 +137,7 @@ def trainSystem():
     Dictionary = cPickle.load(open('dictionary.pkl', 'rb'))
     FilterBank = cPickle.load(open('filterbank.pkl', 'rb'))
     print 'Computing visual words ... ',
-    batchToVisualWords(TrainImagePaths,Classnames,FilterBank,Dictionary,ImageDir,
-        TargetDir)
+    batchToVisualWords(TrainImagePaths,Classnames,FilterBank,Dictionary,ImageDir,TargetDir)
     print 'Done',
     TrainHistograms=createHistograms(len(Dictionary[0]),TrainImagePaths,TargetDir)
     cPickle.dump(TrainHistograms,open('TrainHistograms.pkl', 'wb'))
@@ -148,7 +153,7 @@ def MPWorkerToGetVisualWords(x):
     """
     ImagePath,FilterBank,Dictionary,ImageDir,TargetDir = x
 
-    print 'openning image:',ImagePath[0][0]
+    print 'Opening image:',ImagePath[0][0]
     I = Image.open(os.path.join(ImageDir,ImagePath[0][0]))
     print 'Converting to visual words {0}\n'.format(ImagePath[0][0])
     WordRepresentation = getVisualWords(I, FilterBank, Dictionary)
@@ -170,7 +175,7 @@ def batchToVisualWords(TrainImagePaths,Classnames,FilterBank,Dictionary,ImageDir
 
     x = [(a,FilterBank,Dictionary,ImageDir,TargetDir) for a in TrainImagePaths]
     #pdb.set_trace()
-    p = Pool(None)
+    p = Pool(1)
     p.map(MPWorkerToGetVisualWords, x)
     p.close()
     p.join()
@@ -179,9 +184,9 @@ def createHistograms(DictionarySize,TrainImagePaths,TargetDir):
     """
     concatenate all the histograms from each training image into a single matrix
     """
-    LayerNum=3
+    LayerNum=cfg.NUMBER_OF_LAYER_FOR_SPM
     ImageDir=cfg.IMAGES_PATH
-    OutPutHistograms = np.zeros((DictionarySize*(48),len(TrainImagePaths)))
+    OutPutHistograms = np.zeros((DictionarySize*((4**(LayerNum))/3),len(TrainImagePaths)))
     for i in xrange(len(TrainImagePaths)):
         print 'createHistograms',i
         WordMap = cPickle.load(open(os.path.join(TargetDir,TrainImagePaths[i][0][0]+'.pkl')))
@@ -191,21 +196,22 @@ def createHistograms(DictionarySize,TrainImagePaths,TargetDir):
 
 
 def evaluateRecognitionSystem():
+    print "Evaluating the system..."
     ImageDir=cfg.IMAGES_PATH
     TrainTestMatFile = sio.loadmat('traintest.mat')
-    TestImagePaths = TrainTestMatFile[cfg.TRAIN_IMAGE_PATHS]
+    TestImagePaths = TrainTestMatFile[cfg.TEST_IMAGE_PATHS]
     TrainImageLabels = TrainTestMatFile[cfg.TRAIN_IMAGE_LABELS]
     TestImageLabels  = TrainTestMatFile[cfg.TEST_IMAGE_LABELS]
     Dictionary = cPickle.load(open('dictionary.pkl', 'rb'))
     FilterBank = cPickle.load(open('filterbank.pkl', 'rb'))
     TrainHistograms = cPickle.load(open('TrainHistograms.pkl', 'rb'))
     DictionarySize=len(Dictionary[0])
-    k=2
+    k= cfg.K_FOR_KNN
     ConfusionMatrix=np.zeros((9,9))
-    LayerNum=3
+    LayerNum= cfg.NUMBER_OF_LAYER_FOR_SPM 
     NumOfTestImages=len(TestImagePaths)
     for i in xrange(NumOfTestImages):
-        print i,'/',NumOfTestImages
+        print i+1,'/',NumOfTestImages
         I = Image.open(os.path.join(ImageDir,TestImagePaths[i][0][0]))
         WordMap=getVisualWords(I,FilterBank,Dictionary)
         WordHist=getImageFeaturesSPM(LayerNum,WordMap,DictionarySize)
@@ -213,22 +219,24 @@ def evaluateRecognitionSystem():
         
         ConfusionMatrix[TestImageLabels[i][0]-1,predictedLabel-1]=ConfusionMatrix[TestImageLabels[i][0]-1,predictedLabel-1]+1;
     Accuracy = np.trace(ConfusionMatrix)/np.sum(ConfusionMatrix)
+    print 'ConfusionMatrix=\n',ConfusionMatrix
     print 'Accuracy=',Accuracy
 
 def knnClassify(WordHist,TrainHistograms,TrainImageLabels,k):
     Distances = distanceToSet(WordHist, TrainHistograms)
     I = np.argsort(Distances)[::-1]
-    PredictedLabel= stats.mode(TrainImageLabels[I[1:k]])
+    PredictedLabel= stats.mode(TrainImageLabels[I[0:k]])
     return PredictedLabel[0][0][0]
 
 def visualizeWords():
+    print 'Visualizing the words...'
     ImageDir=cfg.IMAGES_PATH
     TargetDir = cfg.WORDMAP_DIR
     Dictionary = cPickle.load(open('dictionary.pkl', 'rb'))
     TrainTestMatFile = sio.loadmat('traintest.mat')
     TrainImageLabels = TrainTestMatFile[cfg.TRAIN_IMAGE_LABELS]
     TrainImagePaths = TrainTestMatFile[cfg.TRAIN_IMAGE_PATHS]
-    NumOfWords = len(Dictionary[0])
+    DictionarySize = len(Dictionary[0])
     RecomputePatch = True
     if RecomputePatch:
         
@@ -236,10 +244,10 @@ def visualizeWords():
         HalfOfPatchSize=4;
         AverPatchOfWords= []
         
-        for i in xrange(NumOfWords):
+        for i in xrange(DictionarySize):
             AverPatchOfWords.append(np.zeros((PatchSize,PatchSize,3)))
 
-        CountOfWords = np.zeros((NumOfWords,))
+        CountOfWords = np.zeros((DictionarySize,))
         for t in TrainImagePaths:
             I = Image.open(os.path.join(ImageDir,t[0][0]))
             WordMap =  cPickle.load(open(os.path.join(TargetDir,t[0][0]+'.pkl'), 'rb'))
@@ -256,24 +264,24 @@ def visualizeWords():
                     CountOfWords[WordMap[j,k]]+=1
                     AverPatchOfWords[WordMap[j,k]] = AverPatchOfWords[WordMap[j,k]] + I [j-HalfOfPatchSize:j+HalfOfPatchSize+1,k-HalfOfPatchSize:k+HalfOfPatchSize+1,:]
          
-        for i in xrange(NumOfWords):
+        for i in xrange(DictionarySize):
             if (CountOfWords[i]>0):
                 AverPatchOfWords[i] = np.rint(AverPatchOfWords[i]/CountOfWords[i])
         cPickle.dump(AverPatchOfWords,open('AverPatchOfWords.pkl', 'wb'))
     #pdb.set_trace()
     AverPatchOfWords = cPickle.load(open('AverPatchOfWords.pkl', 'rb'))
     NumOfColumTodisplay = 20
-    k =int(np.ceil(NumOfWords/float(NumOfColumTodisplay))) # number of words in each row to display
+    k =int(np.ceil(DictionarySize/float(NumOfColumTodisplay))) # number of words in each row to display
     f, axarr=plt.subplots(k,NumOfColumTodisplay)
 
-    for i in xrange(NumOfWords):
+    for i in xrange(DictionarySize):
         # draw the words
         a = axarr[int(i/NumOfColumTodisplay),int(i%NumOfColumTodisplay)]
         a.imshow(AverPatchOfWords[i])
         a.set_xticks([])
         a.set_yticks([])
    
-    for i in xrange(NumOfWords,k*NumOfColumTodisplay):
+    for i in xrange(DictionarySize,k*NumOfColumTodisplay):
         # fill the empyt sub figure
         a = axarr[int(i/NumOfColumTodisplay),int(i%NumOfColumTodisplay)]
         a.imshow(np.zeros(AverPatchOfWords[0].shape))
@@ -285,5 +293,4 @@ def visualizeWords():
 if __name__ == '__main__':
     trainSystem()
     evaluateRecognitionSystem()
-
     visualizeWords()
